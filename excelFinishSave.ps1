428cf03e9a5eecd9b19507936a6281a136f5e2c0
ishi720 <ishizuka720@gmail.com>
2021-11-01 21:43:47 +0900

    #2 拡張子のチェックを関数化する

--- excelFinishSave.ps1
+++ excelFinishSave.ps1
@@ -3,6 +3,13 @@
 # 「全シート"A1"セルを選択」かつ「一番左のシートを表示」して保存する。
 ###
 
+# 拡張子のチェック関数
+function extensionCheck($fileName) {
+    $extArr = @('.xlsm','.xlsx')
+    $targetExt = [System.IO.Path]::GetExtension("$fileName")
+    return $extArr.Contains($targetExt)
+}
+
 # カレントディレクトリを変数にセット
 $targetDir = [System.IO.Directory]::GetCurrentDirectory()
 
@@ -19,33 +26,33 @@ foreach($item in $itemList) {
     # 処理対象のファイルを変数にセット
     $targetFile = Join-Path $targetDir $item.Name
 
-    # 拡張子が「.xlsx」以外は処理の対象外にする
-    if ([System.IO.Path]::GetExtension($targetFile) -ne ".xlsx"){
-        continue
+
+    # 拡張子のチェック
+    if (extensionCheck($targetFile)) {
+
+        # 処理対象ファイル名表示
+        echo $targetFile
+
+        # エクセルを開く
+        $book = $excel.Workbooks.Open($targetFile)
+
+        # 存在するシート分処理する
+        foreach ($s in $book.sheets){
+            $sheet = $book.Sheets.item($s.name)
+            $sheet.Activate()
+            $sheet.Range("A1").Select()
+            echo $s.name
+        }
+
+        # 一番左のシートをアクティブにする
+        $book.Sheets.item(1).Activate()
+
+        # 保存
+        $book.Save()
+
+        # 閉じる
+        $book.Close()
     }
-
-    # 処理対象ファイル名表示
-    echo $targetFile
-
-    # エクセルを開く
-    $book = $excel.Workbooks.Open($targetFile)
-
-    # 存在するシート分処理する
-    foreach ($s in $book.sheets){
-        $sheet = $book.Sheets.item($s.name)
-        $sheet.Activate()
-        $sheet.Range("A1").Select()
-        echo $s.name
-    }
-
-    # 一番左のシートをアクティブにする
-    $book.Sheets.item(1).Activate()
-
-    # 保存
-    $book.Save()
-
-    # 閉じる
-    $book.Close()
 }
 
 # 後始末
